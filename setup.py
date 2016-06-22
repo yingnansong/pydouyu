adf63d70fa84812041f55283682b1bdfdfe86fd3
Yingnan Song <yingnan.song@funplus.com>
2016-06-22 17:16:26 +0800

    v0.1.2 commit

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 from distutils.core import setup
 setup(
   name = 'douyu',
-  packages = ['douyu', 'douyu.chat'],
-  version = '0.1.1',
+  packages = ['douyu', 'douyu.chat', 'douyu.chat.network'],
+  version = '0.1.2',
   description = 'Python Wrapper for DouyuTV APIs, including support for accessing ChatRoom, e.g. DanMu',
   author = 'Yingnan Song',
   author_email = 'syngod12345@gmail.com',
   url = 'https://github.com/yingnansong/pydouyu',
-  download_url = 'https://github.com/yingnansong/pydouyu/tarball/0.1.1',
+  download_url = 'https://github.com/yingnansong/pydouyu/tarball/0.1.2',
   keywords = ['douyu', 'douyutv', 'danmu', 'chat'],
   classifiers = [],
 )
